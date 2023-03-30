dab77f9fb5a3ddc22be7ba0b25265b0d563636e6
maxi-camonapp <maximiliano@camonapp.com>
2023-03-30 20:25:46 -0300

    feat: complete user controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,13 +1,48 @@
 import { Request, Response } from 'express';
+import { User } from '../models/user.model';
 
 const getUser = async (req: Request, res: Response) => {
-	res.json({ msg: req.params.id });
+	const { id } = req.params;
+	try {
+		const user = await User.findByPk(id);
+		res.status(200).json({ data: user });
+	} catch (error) {
+		res.status(500).json({ data: error });
+	}
 };
+
 const getUsers = async (req: Request, res: Response) => {
-	res.json({ msg: [] });
+	try {
+		const users = User.findAll();
+		res.status(200).json({ data: users });
+	} catch (error) {
+		res.status(500).json({ data: error });
+	}
 };
-const createUser = async (req: Request, res: Response) => {};
-const removeUser = async (req: Request, res: Response) => {};
+
+const createUser = async (req: Request, res: Response) => {
+	const { username, email } = req.body;
+	try {
+		const user = await User.create({ username, email });
+		res.status(200).json({ data: user });
+	} catch (error) {
+		res.status(500).json({ data: error });
+	}
+};
+
+const removeUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	try {
+		const user = await User.destroy({
+			where: {
+				id,
+			},
+		});
+		res.status(200).json({ data: user });
+	} catch (error) {
+		res.status(500).json({ data: error });
+	}
+};
 
 export default {
 	getUsers,
