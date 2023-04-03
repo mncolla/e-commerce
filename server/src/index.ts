3ea277373f30fe2a9573ca11353357709642cae3
maxi-camonapp <maximiliano@camonapp.com>
2023-04-03 04:01:32 -0300

    chore: dotenv config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 // Import the express in typescript file
 import express from 'express';
 import { apiRouter } from './routes';
+import dotenv from 'dotenv';
 
 // Initialize the express engine
 const app: express.Application = express();
 
 // Take a port 3000 for running server.
 const port: number = 3000;
+
+// Environment variables
+dotenv.config();
 
 // To take json format from response.
 app.use(express.json());
@@ -17,5 +21,5 @@ app.use('/api/v1', apiRouter);
 // Server setup
 app.listen(port, () => {
 	console.log(`TypeScript with Express
-         http://localhost:${port}/`);
+         http://localhost:${process.env.PORT || 3000}/`);
 });
