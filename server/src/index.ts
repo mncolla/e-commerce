c73f9d8939bab0d0662cb077920cd00ced0e99a2
maxi-camonapp <maximiliano@camonapp.com>
2023-04-04 22:16:34 -0300

    feat: swagger setup and basic usage

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,35 @@
 import express from 'express';
 import { apiRouter } from './routes';
 import dotenv from 'dotenv';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 // Initialize the express engine
 const app: express.Application = express();
+
+// Swagger config
+const swaggerOptions = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Mi Aplicación API',
+			version: '1.0.0',
+			description: 'Documentación de API para Mi Aplicación',
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000', // Cambiar URL base según tu configuración
+			},
+		],
+	},
+	apis: ['**/*.ts'], // Cambiar ruta de tus rutas según tu configuración
+};
+
+// Swagger init
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// Swagger server
+app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Take a port 3000 for running server.
 const port: number = 3000;
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -2,9 +2,70 @@ import { Router } from 'express';
 import userController from '../controllers/user.controller';
 import authMiddleware from '../middlewares/auth.middleware';
 
-export const userRouter = Router();
+const userRouter = Router();
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Obtener usuarios
+ *     description: Obtener usuarios registrados en la aplicación
+ *     tags:
+ *        - Users
+ *     responses:
+ *       200:
+ *         description: Usuarios obtenidos
+ *       500:
+ *         description: Error al obtener los usuarios
+ */
 userRouter.get('/', [authMiddleware], userController.getUsers);
+
+/**
+ * @swagger
+ * /users/id:
+ *   get:
+ *     summary: Obtener un usuario
+ *     description: Obtener un usuario registrado en la aplicación
+ *     tags:
+ *        - Users
+ *     responses:
+ *       200:
+ *         description: Usuario obtenido
+ *       500:
+ *         description: Error al obtener el usuario
+ */
 userRouter.get('/:id', [authMiddleware], userController.getUser);
+
+/**
+ * @swagger
+ * /users/:
+ *   post:
+ *     summary: Crear un usuario
+ *     description: Registrar un usuario en la aplicacion
+ *     tags:
+ *        - Users
+ *     responses:
+ *       200:
+ *         description: Usuario creado
+ *       500:
+ *         description: Error al crear el usuario
+ */
 userRouter.post('/', [authMiddleware], userController.createUser);
+
+/**
+ * @swagger
+ * /users/id:
+ *   delete:
+ *     summary: Eliminar un usuario
+ *     description: Eliminar un usuario en la aplicacion
+ *     tags:
+ *        - Users
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado
+ *       500:
+ *         description: Error al eliminar el usuario
+ */
 userRouter.delete('/:id', [authMiddleware], userController.removeUser);
+
+export { userRouter };
