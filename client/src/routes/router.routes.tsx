eae78c90acbd4180f03efeda96608b9157de6293
mncolla <maxincolla@gmail.com>
2023-04-12 08:33:32 -0300

    feat: home screen (with products) can be accessed by guests

--- client/src/routes/router.routes.tsx
+++ client/src/routes/router.routes.tsx
@@ -7,17 +7,18 @@ import AuthGuard from "../guards/auth.guard";
 const Signup = lazy(() => import('../pages/Signup'));
 const Login = lazy(() => import('../pages/Login'));
 const Home = lazy(() => import('../pages/Home'));
+const Dashboard = lazy(() => import('../pages/Dashboard'));
 
 const Router = () => {
   return (
     <BrowserRouter>
       <RoutesWithNotFound>
-        <Route path="/" element={<Navigate to={PATH_PAGE.home} />} />
+        <Route path="/" element={<Home />} />
         <Route path={PATH_AUTH.login} element={<Login />} />
         <Route path={PATH_AUTH.signup} element={<Signup />} />
         {/* routes only for logged users */}
         <Route element={<AuthGuard />}>
-          <Route path={PATH_PAGE.home} element={<Home />} />
+          <Route path={PATH_PAGE.dashboard} element={<Dashboard />} />
         </Route>
       </RoutesWithNotFound>
     </BrowserRouter>
